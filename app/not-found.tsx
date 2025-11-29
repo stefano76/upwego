d8dc6014355dc65574e694e93e9d874d932cf2d5
StefanoUpwego <stefano.bonuccelli@upwegodigital.com>
2025-11-29 15:16:06 +1300

    Fix typo in NotFound component text: replace "you're" with "you’re" for improved clarity and consistency.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
           <div className="background-image opacity-0"></div>
           <div className="not-found-number"><h1 className="text-brand-secondary text-8xl font-bold">404</h1></div>
           <h2 className="not-found-title text-brand-tertiary">Oops... Page not found</h2>
-          <p className="not-found-text text-brand-tertiary italic">The page you're looking for doesn't exist or has been moved.</p>
+          <p className="not-found-text text-brand-tertiary italic">The page you’re looking for doesn’t exist or has been moved.</p>
           <Button href="/" variant="blue" className="cta-back-home w-fit mx-auto !mt-[4vh]">Go Back Home</Button>
         </div>
       </div>
